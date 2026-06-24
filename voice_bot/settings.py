ce392962775ac2f46a8452cb503e34e462418f7f
pateltaksh2316 <pateltaksh558@gmail.com>
2026-06-24 14:51:51 +0530

    add favicon

--- voice_bot/settings.py
+++ voice_bot/settings.py
@@ -250,7 +250,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/site/wwwroot/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), 
        # os.path.join(BASE_DIR, 'bot', 'static'),
